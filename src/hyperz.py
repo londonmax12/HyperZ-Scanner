0e05034b8981c27d8c1eb374912005c48ac56365
London Ball <56260362+londonmax12@users.noreply.github.com>
2024-03-21 15:27:12 +1100

    Print header

--- src/hyperz.py
+++ src/hyperz.py
@@ -12,7 +12,29 @@ import sys
 from crawl import crawl
 from header_security import get_insecure_headers
 
+def print_header():
+    print("""
+=====================================================
+                                                     
+  ██░ ██▓██   ██▓ ██▓███  ▓█████  ██▀███  ▒███████▒  
+  ▓██░ ██▒▒██  ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒▒ ▒ ▒ ▄▀░ 
+ ▒██▀▀██░ ▒██ ██░▓██░ ██▓▒▒███   ▓██ ░▄█ ▒░ ▒ ▄▀▒░   
+ ░▓█ ░██  ░ ▐██▓░▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄    ▄▀▒   ░  
+ ░▓█▒░██▓ ░ ██▒▓░▒██▒ ░  ░░▒████▒░██▓ ▒██▒▒███████▒  
+   ▒ ░░▒░▒  ██▒▒▒ ▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░░▒▒ ▓░▒░▒ 
+  ▒ ░▒░ ░▓██ ░▒░ ░▒ ░      ░ ░  ░  ░▒ ░ ▒░░░▒ ▒ ░ ▒  
+  ░  ░░ ░▒ ▒ ░░  ░░          ░     ░░   ░ ░ ░ ░ ░ ░  
+  ░  ░  ░░ ░                 ░  ░   ░       ░ ░      
+         ░ ░                              ░          
+                                                     """)
+    print("HyperZ Web Application Scanner")
+    print("  - Version: 0.1.2")
+    print("  - Developed by Mercury Dev")
+    print("=====================================================\n")
+
 def main():
+    print_header()
+    
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     
     arg_parser = argparse.ArgumentParser(description="HyperZ Web Application Scanner")
@@ -22,16 +44,18 @@ def main():
     args = arg_parser.parse_args()
 
     logging.info(f"Scanning URL: {args.url}")
+    
     logging.info(f"Crawling {args.url}")
 
     links = crawl(args.url, args.depth)
     if (args.verbose):
-        logging.info(f"Found {len(links)} links from crawling")
+        logging.info(f"Found {len(links)} link{'s' if len(links) != 1 else ''} from crawling")
 
     total_severe = 0
     total_moderate = 0
     total_mild = 0
 
+    logging.info(f"Analysing request headers for potential vulnerabilities")
     for link, item in links.items():
         insecure_headers = get_insecure_headers(item["headers"])
         links[link]["insecure_headers"] = insecure_headers
@@ -41,15 +65,17 @@ def main():
         total_mild += len(insecure_headers["Mild"])
     
     total = total_mild + total_moderate + total_severe
+    logging.info(f"Found {total} potential header vulnerabilities across {len(links)} link{'s' if len(links) != 1 else ''}")
+    
+    if total and args.verbose:
+        logging.info(f"  - Severe:   {total_severe}")
+        logging.info(f"  - Moderate: {total_moderate}")
+        logging.info(f"  - Mild:     {total_mild}")
 
-    if total:
-        logging.info(f"Found {total} potential header vulnerabilities across {len(links)} links")
-        logging.info(f"\t| Severe:   {total_severe}")
-        logging.info(f"\t| Moderate: {total_moderate}")
-        logging.info(f"\t| Mild:     {total_mild}")
-
+    save_file = "report.json"
+    logging.info(f"Finished scanning, saving report detail to {save_file}")
     links_dict = {link: {"request_headers": links[link]['headers'], "insecure_headers": links[link]["insecure_headers"]} for link in links}
-    with open("links.json", "w") as f:
+    with open(save_file, "w") as f:
         json.dump(links_dict, f, indent=4)
 
 if __name__ == "__main__":
